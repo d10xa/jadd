84a88ca7537ed90ef5427e6d2c4182272236421c
d10xa <d10xa@mail.ru>
2018-10-25 00:27:54 +0300

    Refactoring proxy settings

--- src/main/scala/ru/d10xa/jadd/JaddRunner.scala
+++ src/main/scala/ru/d10xa/jadd/JaddRunner.scala
@@ -1,8 +1,5 @@
 package ru.d10xa.jadd
 
-import java.net.Authenticator
-import java.net.PasswordAuthentication
-
 import com.typesafe.scalalogging.StrictLogging
 import ru.d10xa.jadd.cli.Cli
 import ru.d10xa.jadd.cli.Command.Repl
@@ -21,46 +18,15 @@ class JaddRunner(
 
     def readAndEvalConfig(args: Array[String]): Config = {
       val config = cli.parse(args)
-      if(config.debug) loggingUtil.enableDebug()
-      if(config.quiet) loggingUtil.quiet()
+      if (config.debug) loggingUtil.enableDebug()
+      if (config.quiet) loggingUtil.quiet()
       config.proxy.foreach(initProxy)
-
       config
     }
 
     def initProxy(proxyStr: String): Unit = {
-
-      def init(host: String, port: String, auth: Option[(String, String)]): Unit = {
-        System.setProperty("http.proxyHost", host)
-        System.setProperty("https.proxyHost", host)
-        System.setProperty("http.proxyPort", port)
-        System.setProperty("https.proxyPort", port)
-        System.setProperty("jdk.http.auth.tunneling.disabledSchemes", "")
-        System.setProperty("jdk.http.auth.proxying.disabledSchemes", "")
-        auth.foreach {
-          case (user, password) =>
-            System.setProperty("http.proxyUser", user)
-            System.setProperty("https.proxyUser", user)
-            System.setProperty("http.proxyPassword", password)
-            System.setProperty("https.proxyPassword", password)
-
-            val authenticator = new Authenticator {
-              override def getPasswordAuthentication: PasswordAuthentication =
-                new PasswordAuthentication(user, password.toCharArray)
-            }
-            Authenticator.setDefault(authenticator)
-        }
-      }
-
-      val userPassHostPort = "(.+):(.+)@(.+):(.+)".r
-      val hostPort = "(.+):(.+)".r
-      val t = proxyStr match {
-        case userPassHostPort(user, password, host, port) =>
-          (host, port, Some((user, password)))
-        case hostPort(host, port) =>
-          (host, port, None)
-      }
-      (init _).tupled(t)
+      val proxySettings = ProxySettings(proxyStr)
+      ProxySettings.set(proxySettings)
     }
 
     def runOnceForRepl(args: Array[String]): Unit =
---
+++ src/main/scala/ru/d10xa/jadd/ProxySettings.scala
@@ -0,0 +1,107 @@
+package ru.d10xa.jadd
+
+import java.net.Authenticator
+import java.net.PasswordAuthentication
+
+import scala.util.matching.Regex
+
+case class ProxySettings(
+  httpHost: Option[String],
+  httpsHost: Option[String],
+  httpPort: Option[String],
+  httpsPort: Option[String],
+  httpProxyUser: Option[String],
+  httpsProxyUser: Option[String],
+  httpProxyPassword: Option[String],
+  httpsProxyPassword: Option[String],
+  tunnelingDisabledSchemes: Option[String],
+  proxyingDisabledSchemes: Option[String],
+  authenticator: Authenticator
+)
+
+object ProxySettings {
+
+  val userPassHostPort: Regex = "(.+):(.+)@(.+):(.+)".r
+  val hostPort: Regex = "(.+):(.+)".r
+
+  val httpProxyHost = "http.proxyHost"
+  val httpsProxyHost = "https.proxyHost"
+  val httpProxyPort = "http.proxyPort"
+  val httpsProxyPort = "https.proxyPort"
+  val httpProxyUser = "http.proxyUser"
+  val httpsProxyUser = "https.proxyUser"
+  val httpProxyPassword = "http.proxyPassword"
+  val httpsProxyPassword = "https.proxyPassword"
+  val tunnelingDisabledSchemes = "jdk.http.auth.tunneling.disabledSchemes"
+  val proxyingDisabledSchemes = "jdk.http.auth.proxying.disabledSchemes"
+
+  def apply(proxy: String): ProxySettings = {
+
+    val (h, p, userAndPassword) = proxy match {
+      case userPassHostPort(user, password, host, port) =>
+        (host, port, Some((user, password)))
+      case hostPort(host, port) =>
+        (host, port, None)
+    }
+
+    val authenticator = new Authenticator {
+      override def getPasswordAuthentication: PasswordAuthentication =
+        new PasswordAuthentication(
+          userAndPassword.map(_._1).getOrElse(""),
+          userAndPassword.map(_._2).getOrElse("").toCharArray
+        )
+    }
+
+    ProxySettings(
+      httpHost = Some(h),
+      httpsHost = Some(h),
+      httpPort = Some(p),
+      httpsPort = Some(p),
+      httpProxyUser = userAndPassword.map(_._1),
+      httpsProxyUser = userAndPassword.map(_._1),
+      httpProxyPassword = userAndPassword.map(_._2),
+      httpsProxyPassword = userAndPassword.map(_._2),
+      tunnelingDisabledSchemes = Some(""),
+      proxyingDisabledSchemes = Some(""),
+      authenticator
+    )
+
+  }
+
+  private def prop(key: String): Option[String] = Option(System.getProperty(key))
+
+  private def prop(key: String, value: Option[String]): Unit = value match {
+    case Some(v) => System.setProperty(key, v)
+    case None => System.clearProperty(key)
+  }
+
+  def get(): ProxySettings = {
+    ProxySettings(
+      httpHost = prop(httpProxyHost),
+      httpsHost = prop(httpsProxyHost),
+      httpPort = prop(httpProxyPort),
+      httpsPort = prop(httpsProxyPort),
+      httpProxyUser = prop(httpProxyUser),
+      httpsProxyUser = prop(httpsProxyUser),
+      httpProxyPassword = prop(httpProxyPassword),
+      httpsProxyPassword = prop(httpsProxyPassword),
+      tunnelingDisabledSchemes = prop(tunnelingDisabledSchemes),
+      proxyingDisabledSchemes = prop(proxyingDisabledSchemes),
+      Authenticator.getDefault
+    )
+  }
+
+  def set(proxySettings: ProxySettings): Unit = {
+    prop(httpProxyHost, proxySettings.httpHost)
+    prop(httpsProxyHost, proxySettings.httpsHost)
+    prop(httpProxyPort, proxySettings.httpPort)
+    prop(httpsProxyPort, proxySettings.httpsPort)
+    prop(httpProxyUser, proxySettings.httpProxyUser)
+    prop(httpsProxyUser, proxySettings.httpsProxyUser)
+    prop(httpProxyPassword, proxySettings.httpProxyPassword)
+    prop(httpsProxyPassword, proxySettings.httpsProxyPassword)
+    prop(tunnelingDisabledSchemes, proxySettings.tunnelingDisabledSchemes)
+    prop(proxyingDisabledSchemes, proxySettings.proxyingDisabledSchemes)
+    Authenticator.setDefault(proxySettings.authenticator)
+  }
+}

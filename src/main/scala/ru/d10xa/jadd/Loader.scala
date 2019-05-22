6c30927aa97e597d72b95054a1f7522f32b1659f
d10xa <d10xa@mail.ru>
2019-05-22 22:40:15 +0300

    Refactoring with cats.data.Ior

--- src/main/scala/ru/d10xa/jadd/Loader.scala
+++ src/main/scala/ru/d10xa/jadd/Loader.scala
@@ -1,18 +1,19 @@
 package ru.d10xa.jadd
 
+import cats.data.Ior
+import cats.data.IorNel
 import cats.data.NonEmptyList
 import cats.effect.Sync
+import cats.implicits._
 import ru.d10xa.jadd.pipelines.Pipeline
 import ru.d10xa.jadd.shortcuts.ArtifactInfoFinder
 import ru.d10xa.jadd.shortcuts.RepositoryShortcuts
 import ru.d10xa.jadd.troubles.ArtifactTrouble
 import ru.d10xa.jadd.versions.ArtifactVersionsDownloader
 import ru.d10xa.jadd.versions.VersionTools
-import cats.implicits._
-import ru.d10xa.jadd.Loader.Result
 
 trait Loader {
-  def load[F[_]: Sync](ctx: Ctx): F[Result]
+  def load[F[_]: Sync](ctx: Ctx): F[IorNel[ArtifactTrouble, List[Artifact]]]
 }
 
 object Loader {
@@ -21,29 +22,35 @@ object Loader {
     repositoryShortcuts: RepositoryShortcuts
   ) extends Loader {
 
-    override def load[F[_]: Sync](ctx: Ctx): F[Result] =
+    override def load[F[_]: Sync](
+      ctx: Ctx): F[IorNel[ArtifactTrouble, List[Artifact]]] =
       Sync[F].delay {
         val artifacts = Pipeline.extractArtifacts(ctx)
         val unshorted: Seq[Artifact] = Utils
           .unshortAll(artifacts.toList, artifactInfoFinder)
         val repositoriesUnshorted: Seq[String] =
           ctx.config.repositories.map(repositoryShortcuts.unshortRepository)
-        val loaded: List[Either[NonEmptyList[ArtifactTrouble], Artifact]] =
-          loadAllArtifacts(unshorted, VersionTools, repositoriesUnshorted)
-        val (a, b) = loaded.separate
-        Result(a, b)
+        loadAllArtifacts(unshorted, VersionTools, repositoriesUnshorted)
       }
 
     def loadAllArtifacts(
       artifacts: Seq[Artifact],
       versionTools: VersionTools,
       repositories: Seq[String]
-    ): List[Either[NonEmptyList[ArtifactTrouble], Artifact]] =
+    ): IorNel[ArtifactTrouble, List[Artifact]] = {
+
+      def loadVersions(a: Artifact): IorNel[ArtifactTrouble, Artifact] =
+        ArtifactVersionsDownloader
+          .loadArtifactVersions(a, repositories, versionTools)
+
+      val initial: IorNel[ArtifactTrouble, List[Artifact]] = Ior.Right(List())
+
       artifacts
-        .map(
-          ArtifactVersionsDownloader
-            .loadArtifactVersions(_, repositories, versionTools))
-        .toList
+        .map(loadVersions)
+        .map(_.map(List(_)))
+        .foldLeft(initial)((a, b) => a.combine(b))
+    }
+
   }
 
   final case class Result(
--- src/main/scala/ru/d10xa/jadd/versions/ArtifactVersionsDownloader.scala
+++ src/main/scala/ru/d10xa/jadd/versions/ArtifactVersionsDownloader.scala
@@ -1,6 +1,9 @@
 package ru.d10xa.jadd.versions
 
 import cats.data.EitherNel
+import cats.data.IorNel
+import cats.data.NonEmptyList
+import cats.implicits._
 import ru.d10xa.jadd.Artifact
 import ru.d10xa.jadd.troubles.ArtifactTrouble
 
@@ -9,19 +12,35 @@ object ArtifactVersionsDownloader {
   def loadArtifactVersions(
     artifact: Artifact,
     configRepositories: Seq[String],
-    versionTools: VersionTools): EitherNel[ArtifactTrouble, Artifact] =
+    versionTools: VersionTools): IorNel[ArtifactTrouble, Artifact] =
     if (artifact.maybeVersion.isDefined) {
-      Right(artifact)
+      artifact.rightIor
     } else {
       val artifactWithRepoList: Stream[Artifact] =
         if (artifact.repository.isDefined) Stream(artifact)
         else
           configRepositories.toStream.map(repo =>
             artifact.copy(repository = Some(repo)))
-      val res = artifactWithRepoList
-        .map(versionTools.loadVersionAndInitLatest)
-      res
+      val res: Seq[EitherNel[ArtifactTrouble, Artifact]] =
+        artifactWithRepoList
+          .map(versionTools.loadVersionAndInitLatest)
+
+      val o: Option[EitherNel[ArtifactTrouble, Artifact]] = res
         .find(_.isRight)
-        .getOrElse(res.head)
+
+      // right.get is safe because of .find(_.isRight)
+      o match {
+        case Some(either) =>
+          either.right.get
+            .rightIor[NonEmptyList[ArtifactTrouble]]
+        case None =>
+          res
+            .collect { case e if e.isLeft => e.left.get }
+            .flatMap(_.toList)
+            .toList
+            .toNel
+            .get
+            .leftIor[Artifact]
+      }
     }
 }

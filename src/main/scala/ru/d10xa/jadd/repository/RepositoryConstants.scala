b8e9cfb8f25e751a7bb7779a1729b65289e40387
d10xa <d10xa@mail.ru>
2020-04-10 22:47:57 +0300

    Change mavenCentral url

--- src/main/scala/ru/d10xa/jadd/repository/RepositoryConstants.scala
+++ src/main/scala/ru/d10xa/jadd/repository/RepositoryConstants.scala
@@ -3,7 +3,7 @@ package ru.d10xa.jadd.repository
 object RepositoryConstants {
   val jcenter: String = "https://jcenter.bintray.com"
   val bintray: String = "https://dl.bintray.com"
-  val mavenCentral: String = "http://central.maven.org/maven2"
+  val mavenCentral: String = "https://repo1.maven.org/maven2"
   val google: String = "https://dl.google.com/dl/android/maven2"
   val sonatype: String = "https://oss.sonatype.org/content/repositories"
 }

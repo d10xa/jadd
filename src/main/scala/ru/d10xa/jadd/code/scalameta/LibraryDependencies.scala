111201b7aa25dfe18c006aeb42eab9a0fc8e5437
Scala Steward <me@scala-steward.org>
2020-10-06 22:40:26 +0200

    Update scalameta to 4.3.24

--- src/main/scala/ru/d10xa/jadd/code/scalameta/LibraryDependencies.scala
+++ src/main/scala/ru/d10xa/jadd/code/scalameta/LibraryDependencies.scala
@@ -4,7 +4,7 @@ import scala.meta.Term
 
 /**
   * Match:
-  * libraryDependencies += "org.scalameta" %% "scalameta" % "4.3.23"
+  * libraryDependencies += "org.scalameta" %% "scalameta" % "4.3.24"
   */
 object LibraryDependencies {
   object moduleIds {
